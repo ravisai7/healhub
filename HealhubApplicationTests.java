24422780c5af38e1f1b098832e6f8cebf920b094
ravisai7 <146260759+ravisai7@users.noreply.github.com>
2025-01-31 12:04:34 -0500

    Add files via upload

---
+++ HealhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot.demo.healhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
